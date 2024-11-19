80c242e08cd0203bb4106a05b4661193e5d0f729
HerHeri <madeheri14@gmail.com>
2024-11-19 07:55:41 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        'voksel-grad-1': "linear-gradient(90deg, #003399, #C289FF)",
+        'voksel-grad-2': "linear-gradient(90deg, #2D56A7, #0E7DC1)",
+        'voksel-blue': "#0E7DC1",
+        'voksel-dark': "#001932"
+      },
+    },
+  },
+  plugins: [
+    require("daisyui")
+  ],
+}
